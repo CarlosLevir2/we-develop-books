c8f90118739c5be87175c27258dc876fbe3bb3b9
Carlos Levir <carloslevirfs@gmail.com>
2021-12-07 15:01:04 -0300

    test: improve and fix tests

---
+++ src/__tests__/components/Card/index.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react';
+import Card from '../../../components/Card';
+import Provider from '../../../Provider';
+import { mangasMock } from '../../../__mocks__/mangas';
+
+const firstManga = mangasMock.top[0];
+
+test('renders card component', () => {
+  render(
+    <Provider>
+      <Card manga={firstManga} />
+    </Provider>
+  );
+  const linkElement = screen.getByText(firstManga.title);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('match snapshot', () => {
+  const { container } = render(
+    <Provider>
+      <Card manga={firstManga} />
+    </Provider>
+  );
+  expect(container).toMatchSnapshot();
+});
