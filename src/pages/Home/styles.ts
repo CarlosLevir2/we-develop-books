49f692c86d7347b71097e0180d020ccbdfadb48a
Carlos Levir <carloslevirfs@gmail.com>
2021-12-07 11:25:22 -0300

    style: fix scrol container background

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
-  height: 100%;
   background-color: ${({ theme }) => theme.colors.background};
   padding: 2% 6%;
 `;
