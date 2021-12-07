865d52c4bc5a7ea13546d861f8e0bbc846a390ec
Carlos Levir <carloslevirfs@gmail.com>
2021-12-07 11:26:48 -0300

    style: fix background scroll

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
+  min-height: 100%;
   background-color: ${({ theme }) => theme.colors.background};
   padding: 2% 6%;
 `;
